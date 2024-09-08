6998883b1c564a64c1a5925397ea7c633e4b73ad
Takahiro Aikou <ta-aikou@heart-musen.com>
2024-09-08 17:44:48 +0900

    initail value account_information

--- pages/marketing/7_invoices.py
+++ pages/marketing/7_invoices.py
@@ -173,7 +173,7 @@ quotation_payment =  st.selectbox(
 )
 
 
-quotation_remark = st.text_area(label="備考", value="ペイペイ銀行ビジネス営業部(005) 普通預金1777361  ハートムセン(ド")
+quotation_remark = st.text_area(label="備考", value=ss.account_information)
 
 col_btn_1, col_btn_2, col_btn_3 = st.columns([0.40, 0.30, 0.30])
 
